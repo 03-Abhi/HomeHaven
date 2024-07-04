c7f84d0bcdd6152345c55ad6d80047479cca1a61
03-Abhi <9818abhi@gmail.com>
2024-07-05 00:01:10 +0530

    update back

--- api/app.js
+++ api/app.js
@@ -7,28 +7,12 @@ import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
-import dotenv from "dotenv";
-import path from "path";
 
-dotenv.config();
-const __dirname = path.resolve();
 const app = express();
 
-const port = process.env.PORT || 8800;
-
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-
-// Route to show server status on a webpage
-app.get("/", (req, res) => {
-  res.send("<h1>Server is running!</h1>");
-});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
@@ -37,12 +21,12 @@ app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
+const port = process.env.PORT || 8800;
+
 app.listen(port, () => {
   console.log("Server is running!");
 });
 
-app.use(express.static(path.join(__dirname, "/client/dist ")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+app.get("/", (req, res) => {
+  res.send("Hello from Api");
 });
